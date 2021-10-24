3f577620e43ee70507785c4d885de476f06f873b
AlekseyLeshko <alekseyleshko@gmail.com>
2021-10-24 15:20:11 +0800

    Use the @alekseyleshko/foo

--- project/baz/src/index.js
+++ project/baz/src/index.js
@@ -1,7 +1,11 @@
+const foo = require('@alekseyleshko/foo');
+
 console.log('baz: module');
 
 const baz = (text) => {
+  foo(test);
   console.log(`baz: module: ${text}`);
 };
+foo('test-baz');
 
 module.exports = baz;
