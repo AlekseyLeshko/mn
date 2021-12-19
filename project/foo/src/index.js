a2ae8ca3bd803dbdd908464c1d4741d605c3f7f7
AlekseyLeshko <alekseyleshko@gmail.com>
2021-12-19 23:43:19 +0800

    Feature y

--- project/foo/src/index.js
+++ project/foo/src/index.js
@@ -3,7 +3,7 @@ const packageJson = require('../package.json');
 
 const foo = (text) => {
   console.log('@foo module version', packageJson.version);
-  console.log('new version');
+  console.log('new version 2');
   console.log(`foo: module: test: ${text}`);
 };
 
